dfce54bb3be2d96824a4d74318d63b93a3684bd0
Fernando Muñoz <fernando1986caballeria@gmail.com>
2026-02-15 19:23:14 -0500

    create: users in bd

--- backend/src/main/java/com/customer_data_consultant/CustomerDataConsultantApplication.java
+++ backend/src/main/java/com/customer_data_consultant/CustomerDataConsultantApplication.java
@@ -7,6 +7,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+import java.util.List;
+
 @SpringBootApplication
 public class CustomerDataConsultantApplication {
 
@@ -16,9 +18,20 @@ public class CustomerDataConsultantApplication {
 
 	@Bean
 	CommandLineRunner init(CustomerRepository repo) {
-		return args -> repo.save(
-				new Customers(null,"hector", "muñoz", "M-1212", "COL-057","HOLA","CC", 1085247127, "CC-25")
-		);
+		return args -> repo.saveAll(List.of(
+
+				new Customers(null,"Hector","Muñoz","M-1","COL-057","M","CC",1085247127,"CC-25"),
+				new Customers(null,"Laura","Gomez","M-2","COL-057","F","CC",1023456789,"CC-25"),
+				new Customers(null,"Carlos","Ramirez","M-3","COL-057","M","CC",1012345678,"CC-25"),
+				new Customers(null,"Andrea","Lopez","M-4","COL-057","F","CC",1098765432,"CC-25"),
+				new Customers(null,"Juan","Perez","M-5","COL-057","M","CC",1009876543,"CC-25"),
+				new Customers(null,"Maria","Castro","M-6","COL-057","F","CC",1034567890,"CC-25"),
+				new Customers(null,"Santiago","Rojas","M-7","COL-057","M","CC",1045678901,"CC-25"),
+				new Customers(null,"Diana","Morales","M-8","COL-057","F","CC",1056789012,"CC-25"),
+				new Customers(null,"Felipe","Torres","M-9","COL-057","M","CC",1067890123,"CC-25"),
+				new Customers(null,"Paula","Vargas","M-10","COL-057","F","CC",1078901234,"CC-25")
+
+		));
 	}
 
 }
